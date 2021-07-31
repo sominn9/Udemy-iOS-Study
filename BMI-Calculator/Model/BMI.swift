f4cda15045b1cac624719a1ea4437131fec191c5
somin <somin3415@naver.com>
2021-07-31 14:06:58 +0900

    Update Project

--- BMI-Calculator/Model/BMI.swift
+++ BMI-Calculator/Model/BMI.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2021 App Brewery. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
