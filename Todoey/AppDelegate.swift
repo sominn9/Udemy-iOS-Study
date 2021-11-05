0214a96818e6400eaf84566d22a1a6b23e9797ac
somin3415 <somin3415@gmail.com>
2021-11-05 22:51:46 +0900

    feat: Change first view controller

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window?.rootViewController = UINavigationController(rootViewController: FolderGridViewController())
+        window?.rootViewController = UINavigationController(rootViewController: MemoListTableViewController())
         return true
     }
 
